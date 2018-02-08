24d6a66934cd38a5bdc143989d98d38ae0b645c2
Einstein <forensicslabnet@gmail.com>
2018-02-08 14:42:56 +0300

    Create mongoDB connection

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 
 [**MongoDB**](https://www.mongodb.com/)
 **[Node Express](https://expressjs.com/)**
+
+
+
+Set environment variables
+ `DB_USER` => Database user
+ `DB_PASS` => Database password
+
--- index.js
+++ index.js
@@ -1,48 +1,59 @@
-const debug = require('debug')('storyofmylife:server');
 const http = require('http');
 const express = require('express');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
+const mongoose = require('mongoose');
 
 let app = express();
 
-//Load middleware
+
+/* Override deprecated promise in mongoose */
+mongoose.Promise = global.Promise;
+
+/* Connect to MongoDB */
+/* Create database 'story-of-my-life' if it doesn't exist */
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const connectionStr = `mongodb://${username}:${password}@localhost/story-of-my-life`;
+mongoose.connect(connectionStr).catch((err) =>{
+    console.log(err);
+});
+
+/* Load middleware */
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
 
-// Routes
+/* Routes */
 app.use('/', index);
 
-// catch 404 and send error message
+/* catch 404 and send error message */
 app.use(function (req, res) {
-    let err = new Error('Not Found');
+    let err = new Error('Route does not exist');
     err.status = 404;
 
-    // set locals, only providing error in development
+    /* set locals, only providing error in development */
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // Return error message
+    /* Return error message */
     res.status(err.status || 500);
-    res.send('Not Found');
+    res.send({
+        errorMsg: 'Not Found',
+        cause: 'Resource does not exists',
+        fix: 'Confirm your URL is typed correctly'
+    });
 });
 
-
-//Set port number
-const port = process.env.PORT || 3050;
-app.set('port', port);
-
-//Create HTTP server.
+/* Create HTTP server.*/
 const server = http.createServer(app);
 
-//Listen on provided port, on all network interfaces.
+/*Set port number and Listen on all network interfaces. */
+const port = process.env.PORT || 3050;
 server.listen(port);
 
-//Event listener for HTTP server "error" event.
+/* Event listener for HTTP server "error" event. */
 server.on('error', () => {
     if (error.syscall !== 'listen') {
         throw error;
@@ -51,7 +62,7 @@ server.on('error', () => {
     const bind = typeof port === 'string' ?
         'Pipe ' + port : 'Port ' + port;
 
-    // handle specific listen errors with friendly messages
+    /* handle specific listen errors with friendly messages */
     switch (error.code) {
         case 'EACCES':
             console.error(bind + ' requires elevated privileges');
@@ -64,13 +75,4 @@ server.on('error', () => {
         default:
             throw error;
     }
-});
-
-//Event listener for HTTP server "listening" event.
-server.on('listening', () => {
-
-    const addr = server.address();
-    const bind = typeof addr === 'string' ?
-        'pipe ' + addr : 'port ' + addr.port;
-    debug('Listening on ' + bind);
 });
